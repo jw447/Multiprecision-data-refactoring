9d01c82df66177db2f89465a50ec25ab7b103126
Jinzhen Wang <jw447@njit.edu>
2023-08-07 18:08:33 -0400

    .

---
+++ build/test/CMakeFiles/test_error_collector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_error_collector.dir/test_error_collector.cpp.o"
+  "test_error_collector"
+  "test_error_collector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_error_collector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
